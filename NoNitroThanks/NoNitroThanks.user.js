ec030d01fba0a07548683536bcc9e4390568dbf1
RayZen <43088283+megamanyoutuber@users.noreply.github.com>
2022-08-31 18:40:33 +0200

    Add files via upload

---
+++ NoNitroThanks/NoNitroThanks.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          NoNitroThanks
+// @version       1.0.2
+// @description   A fork of NoNitroThanks that's modified to work with *Monkey (i hope)
+// @author        RayZen (mod), ImSwordKing (original)
+// @license       urmom
+// @match         https://discord.com/*
+// @resource   IMPORTED_CSS https://github.com/megamanyoutuber/NoNitroThanks-personal-testing/raw/main/NoNitroThanks/NoNitroThanks.style.css
+// @grant      GM_getResourceText
+// @grant      GM_addStyle
+/// ==/UserScript==
+
+(function() {
+    'use strict';
+    const my_css = GM_getResourceText("IMPORTED_CSS");
+    GM_addStyle(my_css);
+})();
